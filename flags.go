353c24498d1ab467097587dbdf225e65cfb9e0c9
Hekmon <edouardhur@gmail.com>
2019-04-17 19:30:43 +0200

    code ok

---
+++ flags.go
@@ -0,0 +1,39 @@
+package bitwise
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// Flags represents a pool of settable flags (64)
+type Flags uint64
+
+// String implements the stdlib Stringer interface
+func (f Flags) String() string {
+	return strconv.FormatUint(uint64(f), 2)
+}
+
+// GoString implements the stdlib GoStringer interface (use "%#v")
+func (f Flags) GoString() string {
+	return fmt.Sprintf("dec(%d)|bin(%s)", f, f)
+}
+
+// IsFlagSet returns true if the flag "flag" is present within "pool" (AND)
+func IsFlagSet(pool, flag Flags) bool {
+	return pool&flag == flag
+}
+
+// RemoveFlag unset "flag" value within "pool" (AND NOT)
+func RemoveFlag(pool, flag Flags) Flags {
+	return pool &^ flag
+}
+
+// SetFlag activate "flag" within "pool" (OR)
+func SetFlag(pool, flag Flags) Flags {
+	return pool | flag
+}
+
+// ToggleFlag switch the state of "flag" within "pool" (XOR)
+func ToggleFlag(pool, flag Flags) Flags {
+	return pool ^ flag
+}
