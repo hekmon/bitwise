b89afa148bd258f62d4314f9e287f55f261f97ec
Hekmon <edouardhur@gmail.com>
2019-04-18 20:18:46 +0200

    regorganize flags

--- flags.go
+++ flags.go
@@ -9,14 +9,13 @@ import (
 // Flags represents a pool of settable flags (64)
 type Flags uint64
 
-// String implements the stdlib Stringer interface
-func (pool Flags) String() string {
-	return strconv.FormatUint(uint64(pool), 2)
-}
+/*
+	Information
+*/
 
-// GoString implements the stdlib GoStringer interface (use "%#v")
-func (pool Flags) GoString() string {
-	return fmt.Sprintf("%064b(%d)", pool, pool)
+// Count returns the number of flags (bits) set in "pool" (Hamming weight)
+func (pool Flags) Count() int {
+	return bits.OnesCount64(uint64(pool))
 }
 
 // IsFlagSet returns true if the flag "flag" is present within "pool" (AND)
@@ -24,10 +23,9 @@ func (pool Flags) IsFlagSet(flag Flags) bool {
 	return pool&flag == flag
 }
 
-// UnsetFlag unset "flag" value within "pool" (AND NOT)
-func (pool Flags) UnsetFlag(flag Flags) {
-	pool = pool &^ flag
-}
+/*
+	Manipulation
+*/
 
 // SetFlag activate "flag" within "pool" (OR)
 func (pool Flags) SetFlag(flag Flags) {
@@ -39,7 +37,21 @@ func (pool Flags) ToggleFlag(flag Flags) {
 	pool = pool ^ flag
 }
 
-// Count returns the number of flags (bits) set in "pool" (Hamming weight)
-func (pool Flags) Count() int {
-	return bits.OnesCount64(uint64(pool))
+// UnsetFlag unset "flag" value within "pool" (AND NOT)
+func (pool Flags) UnsetFlag(flag Flags) {
+	pool = pool &^ flag
+}
+
+/*
+	Printing
+*/
+
+// String implements the stdlib Stringer interface
+func (pool Flags) String() string {
+	return strconv.FormatUint(uint64(pool), 2)
+}
+
+// GoString implements the stdlib GoStringer interface (use "%#v")
+func (pool Flags) GoString() string {
+	return fmt.Sprintf("%064b(%d)", pool, pool)
 }
